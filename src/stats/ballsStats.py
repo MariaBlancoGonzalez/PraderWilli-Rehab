907cdb8adb6670c8e4bc6e7b1d6431a4a482c167
Maria Blanco Gonzalez-Mohino <63149430+MariaBlancoGonzalez@users.noreply.github.com>
2023-05-17 21:32:37 +0200

    stats to object oriented

---
+++ src/stats/ballsStats.py
@@ -0,0 +1,91 @@
+import math
+from utils import *
+import stats.plots as plt
+from stats.calc import *
+from broker import Broker
+
+
+class BallStats:
+    def __init__(self, txt_exer, id_exer, id_user, connection):
+        self.name = txt_exer
+        self.id_exer = id_exer
+        self.id_user = id_user
+
+        self.connect = connection
+
+        self.data = []
+        self.graphs = []
+        self.stats = []
+
+    def create_measures(self):
+        self.get_data()
+        if self.data != []:
+            fecha, errores, aciertos, caidas, tiempo = distribute_data_stats(
+                self.data)
+
+            self.create_stats(fecha, errores, aciertos, caidas, tiempo)
+            self.create_graphs(fecha, errores, aciertos, caidas, tiempo)
+
+    def create_graphs(self, fecha, errores, aciertos, caidas, tiempo):
+        canvas, raw_data = plt.create_groupbar_chart(
+            fecha, errores, aciertos, caidas, tiempo)
+        size = canvas.get_width_height()
+
+        surf = pygame.image.fromstring(raw_data, size, "RGB")
+        self.graphs.append(('Izquierda', surf))
+
+    def create_stats(self, fecha, errores, aciertos, caidas, tiempo):
+        cumulo_errores = [errores[i]+caidas[i] for i in range(len(aciertos))]
+
+        acierto_tiempo, best_score_day, best_score_acierto, time_of_best_day = get_best_day(
+            aciertos, fecha, tiempo)
+        # Este método tambien sirve para el peor
+        errores_tiempo, worst_score_day, worst_score_error, time_of_worst_day = get_best_day(
+            cumulo_errores, fecha, tiempo)
+
+        self.stats.append(
+            ('-Mejor marca correcto/tiempo: ', best_score_acierto))
+        self.stats.append(('-Aciertos/segundos: ', acierto_tiempo))
+        self.stats.append(('-Fecha de esta marca: ', best_score_day))
+        self.stats.append(
+            ('-Tiempo empleado en esta marca: ', time_of_best_day))
+
+        self.stats.append(('-Peor marca correcto/tiempo: ', worst_score_error))
+        self.stats.append(('-Errores/segundos: ', errores_tiempo))
+        self.stats.append(('-Fecha de esta marca: ', worst_score_day))
+        self.stats.append(
+            ('-Tiempo empleado en esta marca: ', time_of_worst_day))
+
+        self.stats.append(('-Errores+Caidas totales: ', sum(cumulo_errores)))
+        self.stats.append(('-Aciertos totales: ', sum(aciertos)))
+
+    def get_data(self):
+        if self.connect == 0:
+            self.get_data_online()
+        else:
+            self.get_data_json()
+
+    def get_data_online(self):
+        broker = Broker()
+        broker.connect()
+        self.data = broker.get_score(self.id_exer, self.id_user, 10)
+        broker.close()
+
+    def get_data_json(self):
+        with open('default.json', 'r') as f:
+            data = json.load(f)
+
+        for score in data['puntuaciones']:
+            date = datetime.datetime.strptime(score['PT_fecha'], '%Y-%m-%d')
+            values = [
+                score['PT_id'],
+                score['PT_A_id'],
+                score['PT_E_id'],
+                date,
+                score['PT_fecha'],
+                score['PT_fallos_izquierda'],
+                score['PT_aciertos_izquierda'],
+                score['PT_fallos_derecha'],
+                score['PT_aciertos_derecha']
+            ]
+            self.data.append(tuple(values))
---
+++ src/stats/diagonalesStats.py
@@ -0,0 +1,97 @@
+import math
+from utils import *
+import stats.plots as plt
+from stats.calc import *
+from broker import Broker
+class DiagonalesStats:
+    def __init__(self, txt_exer, id_exer, id_user, connection):
+        self.name = txt_exer
+        self.id_exer = id_exer
+        self.id_user = id_user
+
+        self.connect = connection
+
+        self.data = []
+        self.graphs = []
+        self.stats = []
+
+    def create_measures(self):
+        self.get_data()
+        if self.data != []:
+            tiempo, izq_errores, izq_aciertos,drcha_errores, drcha_aciertos = distribute_data(self.data)  
+
+            self.create_stats(tiempo, izq_errores, izq_aciertos, drcha_errores, drcha_aciertos)
+            self.create_graphs(tiempo, izq_errores, izq_aciertos, drcha_errores, drcha_aciertos)
+
+    def create_graphs(self, tiempo, izq_errores, izq_aciertos, drcha_errores, drcha_aciertos):
+        canvas_izq, raw_data_izq = plt.create_right_hand_two_lines(izq_errores, izq_aciertos, tiempo, "izquierda")
+        size_izq = canvas_izq.get_width_height()
+
+        surf_izq = pygame.image.fromstring(raw_data_izq, size_izq, "RGB")
+        self.graphs.append(('Izquierda', surf_izq))
+
+        canvas_drcha, raw_data_drcha = plt.create_right_hand_two_lines(drcha_errores, drcha_aciertos, tiempo, "derecha")
+        size_drcha = canvas_drcha.get_width_height()
+
+        surf_drcha = pygame.image.fromstring(raw_data_drcha, size_drcha, "RGB")
+        self.graphs.append(('Derecha', surf_drcha))
+
+    def create_stats(self, tiempo, izq_errores, izq_aciertos, drcha_errores, drcha_aciertos):
+        best_score, best_day = 0, 0
+        media_total_fallos, media_total_aciertos = 0, 0
+        media_errores_d, media_errores_i = 0, 0
+
+        best_score, best_day = get_best_score(self.data)
+        self.stats.append(('-Mejor marca: ', best_score))
+        self.stats.append(('-Fecha: ', best_day))
+
+        media_total_aciertos = calculate_media_total(
+            izq_aciertos, drcha_aciertos
+        )
+        self.stats.append(('-Media de aciertos: ', round(media_total_aciertos,2)))
+
+        media_total_fallos = calculate_media_total(
+            izq_errores, drcha_errores
+        )
+        self.stats.append(('-Media de errores : ', round(media_total_fallos,2)))
+        
+        media_aciertos_d = calculate_media_parte(drcha_aciertos)
+        media_aciertos_i = calculate_media_parte(izq_aciertos)
+        self.stats.append(('-Media de aciertos derecha: ', round(media_aciertos_d,2)))
+        self.stats.append(('-Media de aciertos izquierda: ', round(media_aciertos_i,2)))
+
+        media_errores_d = calculate_media_parte(drcha_errores)
+        media_errores_i = calculate_media_parte(izq_errores)
+        self.stats.append(('-Media de errores derecha: ', media_errores_d))
+        self.stats.append(('-Media de errores izquierda: ', media_errores_i))
+
+    def get_data(self):
+        if self.connect == 0:
+            self.get_data_online()
+        else:
+            self.get_data_json()
+
+    def get_data_online(self):
+        broker = Broker()
+        broker.connect()
+        self.data = broker.get_score(self.id_exer, self.id_user, 10)
+        broker.close()
+
+    def get_data_json(self):
+        with open('default.json', 'r') as f:
+            data = json.load(f)
+
+        for score in data['puntuaciones']:
+            date = datetime.datetime.strptime(score['PT_fecha'], '%Y-%m-%d')
+            values = [
+                score['PT_id'],
+                score['PT_A_id'],
+                score['PT_E_id'],
+                date,
+                score['PT_tiempo'],
+                score['PT_fallos_izquierda'],
+                score['PT_aciertos_izquierda'],
+                score['PT_fallos_derecha'],
+                score['PT_aciertos_derecha']
+            ]
+            self.data.append(tuple(values))
